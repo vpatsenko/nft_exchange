8607b4a21022be523d7b3fa626ac36cf81ec088b
Vladimir Patsenko <v@patsenko.ru>
2022-07-11 15:37:11 +0300

    init

---
+++ DAO/contracts/test.js
@@ -0,0 +1,25 @@
+const ethers = require("ethers");
+
+async function deploy(){
+// const factory = new ContractFactory(contractAbi, contractByteCode);
+    // const factory = await new ethers.ContractFactory(abi, bytecode)
+    // const dex = await factory.attach("0xd9145CCE52D386f254917e481eB44e9943F39138")
+
+    return new Promise((res,rej)=>{res(ret)})
+}
+
+
+function main() {
+    return new Promise((res, rej) =>{
+        const depl = deploy().then(
+			console.log(res)
+		)
+        res()
+    })
+}
+
+
+
+main()
+.then(
+    ()=>{console.log("sex")})
